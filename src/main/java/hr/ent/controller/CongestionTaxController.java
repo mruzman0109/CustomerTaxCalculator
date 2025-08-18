a63865a96be00e9cf4303bc8c174546d28950eb3
Marko Ružman <ruzman.marko@gmail.com>
2025-08-18 14:05:48 +0200

    Add call from facade to service

--- src/main/java/hr/ent/controller/CongestionTaxController.java
+++ src/main/java/hr/ent/controller/CongestionTaxController.java
@@ -1,8 +1,13 @@
 package hr.ent.controller;
 
+import hr.ent.beans.VehiclePass;
 import hr.ent.services.CongestionTaxService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/tax")
@@ -12,5 +17,10 @@ public class CongestionTaxController {
 
     public CongestionTaxController(CongestionTaxService congestionTaxService) {
         this.congestionTaxService = congestionTaxService;
+    }
+
+    @PostMapping("/calculate")
+    public float calculateTax(@RequestBody List<VehiclePass> passes) {
+        return congestionTaxService.calculateTax(passes);
     }
 }
--- src/main/java/hr/ent/services/CongestionTaxService.java
+++ src/main/java/hr/ent/services/CongestionTaxService.java
@@ -1,11 +1,14 @@
 package hr.ent.services;
 
+import hr.ent.beans.VehiclePass;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CongestionTaxService {
 
-    public float calculateTax() {
+    public float calculateTax(List<VehiclePass> vehiclePasses) {
         return 0.0f;
     }
 }
